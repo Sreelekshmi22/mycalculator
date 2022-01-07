5b7c5da17531ee0c75e26507c90690ff3019c32d
sreelekshmi <srilaxmynair@gmail.com>
2022-01-07 10:59:25 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function displaynum(num){
+    result.value+=num
+}
+function clearBox(){
+    result.value=""
+}
+function evaluateExpression(){
+    result.value=eval(result.value)
+}
+function deletenum(){
+    result.value=result.value.slice(0,-1)
+}
